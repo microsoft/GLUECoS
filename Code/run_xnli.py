681c872169b839fb42f9601dc720d23d5a2ecd7b
Anirudh Srinivasan <t-ansrin@microsoft.com>
2020-07-17 00:08:29 +0530

    Update max seq len for NLI to prevent crashing

--- Code/run_xnli.py
+++ Code/run_xnli.py
@@ -174,7 +174,7 @@ def train(args, train_dataset, model, tokenizer):
                         args.local_rank == -1 and args.evaluate_during_training
                     ):  # Only evaluate when single GPU otherwise metrics may not average well
                         results = evaluate(args, model, tokenizer)
-                        for key, value in results.items():
+                        # for key, value in results.items():
                     #         tb_writer.add_scalar("eval_{}".format(key), value, global_step)
                     # tb_writer.add_scalar("lr", scheduler.get_lr()[0], global_step)
                     # tb_writer.add_scalar("loss", (tr_loss - logging_loss) / args.logging_steps, global_step)
--- Code/train_nli.sh
+++ Code/train_nli.sh
@@ -7,7 +7,7 @@ OUT_DIR=${5:-"$REPO/Results"}
 
 EPOCH=5
 BATCH_SIZE=8
-MAX_SEQ=1024
+MAX_SEQ=512
 
 python $PWD/Code/run_xnli.py \
   --data_dir $DATA_DIR/$TASK \
